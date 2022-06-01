7381680f5c5306ff02c5d0b7e08a24537e9a1d20
Richard Leven <richard.leven@udo.edu>
2022-06-01 08:48:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# df = pd.read_csv('data/archive/REMS_Mars_Dataset.csv', 
+#                  encoding='utf-8', 
+#                  comment='#',
+#                  sep=',')
+
+# print(df.columns)
+
+# plt.scatter(df['max_ground_temp(°C)'], df['mean_pressure(Pa)'])
+# plt.xlabel('T')
+# plt.ylabel('P')
+# plt.show()
