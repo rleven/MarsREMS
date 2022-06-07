ebe9db00b0fb13cef75726465f3ebc6f12ac6478
Richard Leven <richard.leven@udo.edu>
2022-06-07 12:22:36 +0200

    Cleaned the df of unavailabl values & useless cols

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# df = pd.read_csv('data/archive/REMS_Mars_Dataset.csv', 
-#                  encoding='utf-8', 
-#                  comment='#',
-#                  sep=',')
-
-# print(df.columns)
-
-# plt.scatter(df['max_ground_temp(°C)'], df['mean_pressure(Pa)'])
-# plt.xlabel('T')
-# plt.ylabel('P')
-# plt.show()
